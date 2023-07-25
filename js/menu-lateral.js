c083bddcb38c6a973eb2a4d20f649b5c731b998c
Wesleybezerra3 <wesleybezerra770@gmail.com>
2023-07-25 18:01:00 -0300

    alguns erros corrigidos

--- js/menu-lateral.js
+++ js/menu-lateral.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const openModalButton = document.querySelector("#openNav");
-    const closeModalButton = document.querySelector("#closeNav");
-    const nav = document.querySelector("nav");
-    const fade = document.querySelector("#fade");
+  const openModalButton = document.querySelector("#openNav");
+  const closeModalButton = document.querySelector("#closeNav");
+  const nav = document.querySelector("nav");
+  const fade = document.querySelector("#fade");
+  const body = document.querySelector('body');
+  const toggleModal = () => {
+    nav.classList.toggle("hide");
+    fade.classList.toggle("hide");
+    body.classList.toggle("disable-scrol")
+  };
 
-    const toggleModal = () => {
-      nav.classList.toggle("hide");
-      fade.classList.toggle("hide");
-    };
+  [openModalButton, closeModalButton, fade].forEach((el) => {
+    
+    el.addEventListener("click", () => toggleModal());
 
-    [openModalButton, closeModalButton].forEach((el) => {
-      el.addEventListener("click", () => toggleModal());
-    });
   });
+});
